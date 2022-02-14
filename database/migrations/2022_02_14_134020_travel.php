55cf54419927584109667b0cec89fd4f400f6e06
Lore0905 <lore.castelli4@gmail.com>
2022-02-14 15:28:15 +0100

    migration

---
+++ database/migrations/2022_02_14_134020_travel.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Travel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel_agency', function (Blueprint $table) {
+            $table->id();
+            $table->string('place', 100);
+            $table->date('start');
+            $table->date('finish');
+            $table->decimal('price', 8, 2);
+            $table->integer('days');
+            $table->string('hotel_name', 100);
+            $table->string('hotel_adress', 100);
+            $table->text('description')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel_agency');
+    }
+}
